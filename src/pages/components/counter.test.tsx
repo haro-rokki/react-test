97ce4933193c2b79c6243d28a77ca1454577c52c
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2020-12-05 16:38:38 +0900

    test追加

--- src/pages/components/counter.test.tsx
+++ src/pages/components/counter.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, within } from '@testing-library/react';
 
 import { Counter } from './counter';
 
@@ -7,9 +7,46 @@ describe('カウンターのテスト', () => {
   it('初期表示はカウントが0である', () => {
     render(<Counter />);
 
-    expect(screen.getByText('0')).toBeInTheDocument();
-    expect(screen.queryByText('1')).not.toBeInTheDocument();
-    expect(screen.getByText('+')).toBeInTheDocument();
-    expect(screen.getByText('-')).toBeInTheDocument();
+    const normal = within(screen.getByTestId('normal'));
+    expect(normal.getByText('0')).toBeInTheDocument();
+    expect(normal.queryByText('1')).not.toBeInTheDocument();
+    expect(normal.getByText('+')).toBeInTheDocument();
+    expect(normal.getByText('-')).toBeInTheDocument();
+  });
+
+  it('+を押すと1増えること', () => {
+    render(<Counter />);
+
+    const normal = within(screen.getByTestId('normal'));
+
+    fireEvent.click(normal.getByText('+'));
+
+    expect(normal.getByText('1')).toBeInTheDocument();
+    expect(normal.getByText('+')).toBeInTheDocument();
+    expect(normal.getByText('-')).toBeInTheDocument();
+  });
+
+  it('-を押すと1減ること', () => {
+    render(<Counter />);
+
+    const normal = within(screen.getByTestId('normal'));
+
+    fireEvent.click(normal.getByText('-'));
+
+    expect(normal.getByText('-1')).toBeInTheDocument();
+    expect(normal.getByText('+')).toBeInTheDocument();
+    expect(normal.getByText('-')).toBeInTheDocument();
+  });
+
+  it('+10を押すと10増えること', () => {
+    render(<Counter />);
+
+    const ten = within(screen.getByTestId('ten'));
+
+    fireEvent.click(ten.getByText('+'));
+
+    expect(ten.getByText('10')).toBeInTheDocument();
+    expect(ten.getByText('+')).toBeInTheDocument();
+    expect(ten.getByText('-')).toBeInTheDocument();
   });
 });
--- src/pages/components/counter.tsx
+++ src/pages/components/counter.tsx
@@ -2,19 +2,34 @@ import React, { useState } from 'react';
 
 export const Counter: React.FC = () => {
   const [count, setCount] = useState(0);
+  const [countTen, setCountTen] = useState(0);
 
   const increment = () => setCount((c) => c + 1);
   const decrement = () => setCount((c) => c - 1);
 
+  const incrementTen = () => setCountTen((c) => c + 10);
+  const decrementTen = () => setCountTen((c) => c - 10);
+
   return (
-    <div>
-      <span>{count}</span>
-      <button type="button" onClick={increment}>
-        +
-      </button>
-      <button type="button" onClick={decrement}>
-        -
-      </button>
-    </div>
+    <>
+      <div data-testid="normal">
+        <span>{count}</span>
+        <button type="button" onClick={increment}>
+          +
+        </button>
+        <button type="button" onClick={decrement}>
+          -
+        </button>
+      </div>
+      <div data-testid="ten">
+        <span>{countTen}</span>
+        <button type="button" onClick={incrementTen}>
+          +
+        </button>
+        <button type="button" onClick={decrementTen}>
+          -
+        </button>
+      </div>
+    </>
   );
 };
